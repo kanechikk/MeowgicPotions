d4d12fbacd5d9c82a6ebdecdecd492ea7bed73b1
vladikNovla <zyymer20001000@gmail.com>
2024-12-01 20:22:17 +0300

    Player Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,32 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public CharacterController characterController;
+    private CharacterController m_characterController;
     [SerializeField]
-    private float speedMove = 5f;
+    private float m_speedMove = 5f;
     [SerializeField]
-    private float speedRotation = 5f;
+    private float m_speedRotation = 800f;
 
-    public void Move(Vector2 dir)
+    private void Awake()
     {
+        m_characterController = GetComponent<CharacterController>();
+    }
 
+    public void Move(Vector2 input) // Метод передачи импута в SimpleMove и вращения персонажа в направлении движения
+    {
+        if (m_characterController)
+        {
+            Vector3 dir = new Vector3(input.x, 0f, input.y);
+            m_characterController.SimpleMove(dir * m_speedMove);
+
+            if (input.sqrMagnitude > 0)
+            {
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(dir), m_speedRotation * Time.deltaTime);
+                Debug.Log(transform.rotation);
+            }
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,24 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlaeyrConroller : MonoBehaviour
 {
     public Player player;
-    public InputActionMap inputActionMap;
+    public InputActionAsset inputActions;
+    private InputAction m_moveAction;
+
+    private void Start()
+    {
+        var map = inputActions.FindActionMap("Player");
+        map.Enable();
+
+        m_moveAction = map.FindAction("Move");
+    }
+
+    private void Update()
+    {
+        Vector2 move = m_moveAction.ReadValue<Vector2>();
+        player.Move(move);
+    }
 }
