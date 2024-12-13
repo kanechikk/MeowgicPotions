c0b8797d568b321b5efa59203da9f86443d59d04
Anastasia <kanechik.k@gmail.com>
2024-12-13 18:04:43 +0300

    DataStorage created

--- Assets/Scripts/Potions/Item.cs
+++ Assets/Scripts/Potions/Item.cs
@@ -2,9 +2,8 @@ using UnityEngine;
 
 public abstract class Item : ScriptableObject
 {
-    public int id;
     public string itemName = "ItemSample";
     public float price = 100.0f;
     public Sprite icon;
-    public GameObject prefab;
+    //public GameObject prefab;
 }
