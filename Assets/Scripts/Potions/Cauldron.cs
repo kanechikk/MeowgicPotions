d6c4bef34115b6e3d06c5b21c60a5626fb576ee9
Lera <Lera.53rus@yandex.ru>
2024-12-14 16:59:34 +0300

    Сounting elements

--- Assets/Scripts/Potions/Cauldron.cs
+++ Assets/Scripts/Potions/Cauldron.cs
@@ -1,14 +1,42 @@
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Cauldron: MonoBehaviour
 {
     private List<Ingredient> addedIngredients = new List<Ingredient>();
+    private int aquaCount = 0;
+    private int ignisCount = 0;
+    private int terraCount = 0;
+    private int aerCount = 0;
+    private int solarCount = 0;
+
     public void AddIngredient(Ingredient ingredient)
     {
         if (ingredient != null)
         {
             addedIngredients.Add(ingredient);
+            foreach (ElementType element in ingredient.elementsHaving)
+            {
+                switch (element)
+                {
+                    case ElementType.Aqua:
+                        aquaCount++;
+                        break;
+                    case ElementType.Ignis:
+                        ignisCount++;
+                        break;
+                    case ElementType.Terra:
+                        terraCount++;
+                        break;
+                    case ElementType.Aer:
+                        aerCount++;
+                        break;
+                    case ElementType.Solar:
+                        solarCount++;
+                        break;
+                }
+            }
             Debug.Log($"Добавлен ингредиент: {ingredient.itemName}");
         }
     }
@@ -23,7 +51,28 @@ public class Cauldron: MonoBehaviour
         if (ingredient != null)
         {
             addedIngredients.Remove(ingredient);
-            Debug.Log($"Удален ингредиент: {ingredient.itemName}");
+            foreach (ElementType element in ingredient.elementsHaving)
+            {
+                switch (element)
+                {
+                    case ElementType.Aqua:
+                        aquaCount--;
+                        break;
+                    case ElementType.Ignis:
+                        ignisCount--;
+                        break;
+                    case ElementType.Terra:
+                        terraCount--;
+                        break;
+                    case ElementType.Aer:
+                        aerCount--;
+                        break;
+                    case ElementType.Solar:
+                        solarCount--;
+                        break;
+                }
+            }
+            Debug.Log($"Удален ингредиент: {ingredient.itemName}"); 
         }
     }
 
@@ -31,9 +80,14 @@ public class Cauldron: MonoBehaviour
     {
         Debug.Log("Содержимое котла:");
         foreach (var ingredient in addedIngredients)
-        {
             Debug.Log($"- {ingredient.itemName}");
-        }
+        
+        Debug.Log("Свойства в котле:");
+        Debug.Log($"Aqua: {aquaCount}");
+        Debug.Log($"Ignis: {ignisCount}");
+        Debug.Log($"Terra: {terraCount}");
+        Debug.Log($"Aer: {aerCount}");
+        Debug.Log($"Solar: {solarCount}");
     }
 }
 
