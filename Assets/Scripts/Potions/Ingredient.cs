8d24047b215d825518688ac5a85d0d6315d19e8f
vladikNovla <zyymer20001000@gmail.com>
2024-12-14 16:12:33 +0300

    ScriptableObjects added
    data about ingredients and potions added

--- Assets/Scripts/Potions/Ingredient.cs
+++ Assets/Scripts/Potions/Ingredient.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Ingredient", menuName = "Scriptable Objects/Ingredient")]
 public class Ingredient : Item
 {
-    public List<ElementType> elementsHaving = new List<ElementType>();
+    //public List<ElementType> elementsHaving = new List<ElementType>();
+    public int aqua;
+    public int terra;
+    public int solar;
+    public int ignis;
+    public int aer;
+    Dictionary<string, int> elements;
+
+    public void AddToDictionary()
+    {
+        elements = new Dictionary<string, int>()
+        {
+            {"aqua", aqua},
+            {"terra", terra},
+            {"solar", solar},
+            {"ignis", ignis},
+            {"aer", aer}
+        };
+    }
 }
--- Assets/Scripts/Potions/Potion.cs
+++ Assets/Scripts/Potions/Potion.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Potion", menuName = "Scriptable Objects/Potion")]
 public class Potion : Item
 {
-    public List<ElementType> elementsNeedded = new List<ElementType>();
+    //public List<ElementType> elementsNeedded = new List<ElementType>();
+    public int aqua;
+    public int terra;
+    public int solar;
+    public int ignis;
+    public int aer;
+    Dictionary<string, int> elements;
+
+    public void AddToDictionary()
+    {
+        elements = new Dictionary<string, int>()
+        {
+            {"aqua", aqua},
+            {"terra", terra},
+            {"solar", solar},
+            {"ignis", ignis},
+            {"aer", aer}
+        };
+    }
 }
