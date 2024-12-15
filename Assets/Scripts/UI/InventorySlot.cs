e9cb1e27406df3f3e2f84bd6cd2728e8f21938e3
vladikNovla <zyymer20001000@gmail.com>
2024-12-15 21:18:14 +0300

    Two items in one slot fixed

--- Assets/Scripts/UI/InventorySlot.cs
+++ Assets/Scripts/UI/InventorySlot.cs
@@ -5,8 +5,11 @@ public class InventorySlot : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData)
     {
-        GameObject droped = eventData.pointerDrag;
-        DraggableItem draggableItem = droped.GetComponent<DraggableItem>();
-        draggableItem.parentAfterDrag = transform;
+        if (transform.childCount == 0)
+        {
+            GameObject droped = eventData.pointerDrag;
+            DraggableItem draggableItem = droped.GetComponent<DraggableItem>();
+            draggableItem.parentAfterDrag = transform;
+        }
     }
 }
