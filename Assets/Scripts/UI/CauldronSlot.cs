d750d2743db2e797a3d071c2d4a4248fc06a7d80
Анастасия <kanechik.k@gmail.com>
2024-12-23 17:09:59 +0300

    added temporary sprites for ingredients

---
+++ Assets/Scripts/UI/CauldronSlot.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CauldronSlot : MonoBehaviour, IDropHandler
+{
+    
+    public event Action<Ingredient> onAddIngredient;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (transform.childCount == 0)
+        {
+            GameObject dropped = eventData.pointerDrag;
+            DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
+            draggableItem.parentAfterDrag = transform;
+
+            onAddIngredient?.Invoke(draggableItem.item);
+        }
+        else
+        {
+            GameObject dropped = eventData.pointerDrag;
+            DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
+
+            GameObject current = transform.GetChild(0).gameObject;
+            DraggableItem currentDraggable = current.GetComponent<DraggableItem>();
+
+            currentDraggable.transform.SetParent(draggableItem.parentAfterDrag);
+            draggableItem.parentAfterDrag = transform;
+        }
+    }
+
+
+}
--- Assets/Scripts/UI/DraggableItem.cs
+++ Assets/Scripts/UI/DraggableItem.cs
@@ -1,12 +1,25 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 
 public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    public Ingredient item;
     private Image image;
     [HideInInspector] public Transform parentAfterDrag;
+
+    private void Start()
+    {
+        InitialiseItem(item);
+    }
+
+    public void InitialiseItem(Ingredient newItem)
+    {
+        item = newItem;
+        image.sprite = newItem.icon;
+    }
 
     private void Awake()
     {
