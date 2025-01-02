cd05611e197317009d7ad0d32a6ee863e3a13cab
vladikNovla <zyymer20001000@gmail.com>
2025-01-02 14:48:18 +0300

    ShioppingState WIP

--- Assets/Scripts/States/ShoppingState.cs
+++ Assets/Scripts/States/ShoppingState.cs
@@ -7,16 +7,12 @@ public class ShoppingState : MonoBehaviour
     private Inventory shop;
     private Ingredient[] m_allIngredients;
     [SerializeField] private GameObject[] m_IngredientPrefabs;
-    //private Potion[] m_allPotions;
     //private Seed[] m_allSeeds;
-    //private bool potions_stocked;
     private bool ingredients_stocked;
     private bool seeds_stocked;
-    //public GameObject potionsPanel;
-    //private Transform[] potionPanelSlots;
     public GameObject ingredientsPanel;
-    private Transform[] ingredientPanelSlots;
     public SlotsFilling slotsFilling;
+    public GameObject linePrefab;
     private int page;
     
 
@@ -26,45 +22,41 @@ public class ShoppingState : MonoBehaviour
     }
     private void OnEnable()
     {
+        if (ingredients_stocked || seeds_stocked)
+        {
+            FillTheShop();
+        }
+    }
+    private void FillTheShop()
+    {
         if (!ingredients_stocked)
         {
+            // Заполнение массива ингредиентов из папки Resources
             m_allIngredients = Resources.LoadAll<Ingredient>("ScriptableObjects/Ingredients");
+            int ingsCount = 0;
+            // Заполняем инвентарь магазина
             foreach (Ingredient ingredient in m_allIngredients)
             {
                 shop.AddItem(ingredient);
+                ingsCount++;
             }
-            ingredientPanelSlots = ingredientsPanel.GetComponentsInChildren<Transform>().Skip(1).ToArray(); // Скипает первый элемент массива, так как он туда закидывает еще трансформ бэкграунда магазина
 
-            for (int i = 0; i < Math.Min(ingredientPanelSlots.Length, m_IngredientPrefabs.Length); i++)
+            for (int i = 0; i < ingsCount; i++)
             {
-                slotsFilling.FillSlot(m_IngredientPrefabs[i], ingredientPanelSlots[i].transform);
-                Debug.Log(ingredientPanelSlots[i].name);
-                Debug.Log(i);
+                GameObject newLine = Instantiate(linePrefab, ingredientsPanel.transform);
             }
 
             ingredients_stocked = true;
         }
-        /*if (!ingredients_stocked)
-        {
-            m_allPotion = Resources.LoadAll<Potion>("ScriptableObjects/Potions");
-            foreach (Potion potion in m_allPotion)
-            {
-                shop.AddItem(potion);
-            }
-            ingredientPanelSlots = ingredientsPanel.GetComponentsInChildren<Transform>();
-
-            foreach (Transform slot in ingredientPanelSlots)
-            {
-
-            }
-
-            ingredients_stocked = true;
-        }*/
         if (!seeds_stocked)
         {
             // m_allSeeds = Resources.LoadAll<Seed>("ScriptableObjects/Seeds");
 
             // seeds_stocked = true;
         }
+    }
+    private void FillOnePage(GameObject page)
+    {
+
     }
 }
