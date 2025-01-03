0ff698cef9f1978f6de9845565e9f7835302aecd
The Meowy <meowmeowingstorage00@gmail.com>
2025-01-04 00:58:15 +0300

    hide/show func and no offscreen correction

--- Assets/Scripts/UI/TooltipUI.cs
+++ Assets/Scripts/UI/TooltipUI.cs
@@ -14,7 +14,7 @@ public class TooltipUI : MonoBehaviour
     void Awake()
     {
         tooltipTransform = GetComponent<RectTransform>();
-        SetText("test");
+        SetText("test \n \n \n \n");
     }
 
     private void SetText(string text)
@@ -27,16 +27,41 @@ public class TooltipUI : MonoBehaviour
     {
         Vector2 position = Input.mousePosition / canvasTransform.localScale.y;
 
-        if (position.x + bgTransform.rect.width > canvasTransform.rect.width)
+        //thing that stops tooltip from going offscreen (штука чтобы тултип не уходил за экран)
+        if (position.x + bgTransform.rect.width > canvasTransform.rect.width) //right
         {
             position.x = canvasTransform.rect.width - bgTransform.rect.width;
         }
-        if (position.y + bgTransform.rect.height > canvasTransform.rect.height)
+        if (position.x < 0) //left
+        {
+            position.x = 0;
+        }
+
+        if (position.y + bgTransform.rect.height > canvasTransform.rect.height) //up
         {
             position.y = canvasTransform.rect.height - bgTransform.rect.height;
         }
+        if (position.y < 0) //down
+        {
+            position.y = 0;
+        }
 
         tooltipTransform.anchoredPosition = position;
+        //extra thoughts: here I tried to flip tooltip to be on bottom right from curson cuz its better lookin
+        //do not do that
+        //it WILL NOT work
+        //fuck anchor points fuck reversed coordinate bullshit it does
     }
 
+    public void ShowTooltip(string tooltipText)
+    {
+        gameObject.SetActive(true);
+        SetText(tooltipText);
+    }
+
+    public void HideTooltip(string tooltipText)
+    {
+        gameObject.SetActive(false);
+        SetText(tooltipText);
+    }
 }
