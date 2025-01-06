10aed8c84aa306a5308467ea0fbb4daead94877a
The Meowy <meowmeowingstorage00@gmail.com>
2025-01-06 18:17:03 +0300

    add getting text 4 item

--- Assets/Scripts/UI/TooltipUI.cs
+++ Assets/Scripts/UI/TooltipUI.cs
@@ -3,18 +3,17 @@ using UnityEngine;
 
 public class TooltipUI : MonoBehaviour
 {
-    //static instance cuz theres only 1 at a time (статичск штука т к только 1 существует одновременно)
-    public static TooltipUI instance;
-    [SerializeField] private RectTransform bgTransform;
-    [SerializeField] private TextMeshProUGUI textMeshProUGUI;
-    [SerializeField] private RectTransform canvasTransform;
+    private RectTransform bgTransform;
+    private TextMeshProUGUI textMeshProUGUI;
     private RectTransform tooltipTransform;
     public Vector2 padding = new Vector2(10, 10);
+    [SerializeField] private RectTransform canvasTransformArea;
 
     void Awake()
     {
-        tooltipTransform = GetComponent<RectTransform>();
-        instance = this;
+        bgTransform = gameObject.GetComponentsInChildren<RectTransform>()[1];
+        textMeshProUGUI = gameObject.GetComponentsInChildren<TextMeshProUGUI>()[0];
+        tooltipTransform = gameObject.GetComponent<RectTransform>();
         SetText("test \n \n \n \n");
         HideTooltip();
     }
@@ -27,51 +26,40 @@ public class TooltipUI : MonoBehaviour
     }
     void Update()
     {
-        Vector2 position = Input.mousePosition / canvasTransform.localScale.y;
+        Vector2 position = Input.mousePosition / canvasTransformArea.localScale.y;
 
         //thing that stops tooltip from going offscreen (штука чтобы тултип не уходил за экран)
-        if (position.x + bgTransform.rect.width > canvasTransform.rect.width) //right
+        if (position.x + bgTransform.rect.width > canvasTransformArea.rect.width) //right
         {
-            position.x = canvasTransform.rect.width - bgTransform.rect.width;
+            position.x = canvasTransformArea.rect.width - bgTransform.rect.width;
         }
+        
         if (position.x < 0) //left
         {
             position.x = 0;
         }
 
-        if (position.y + bgTransform.rect.height > canvasTransform.rect.height) //up
+        if (position.y + bgTransform.rect.height > canvasTransformArea.rect.height) //up
         {
-            position.y = canvasTransform.rect.height - bgTransform.rect.height;
+            position.y = canvasTransformArea.rect.height - bgTransform.rect.height;
         }
+
         if (position.y < 0) //down
         {
             position.y = 0;
         }
 
         tooltipTransform.anchoredPosition = position;
-        //extra thoughts: here I tried to flip tooltip to be on bottom right from curson cuz its better lookin
-        //do not do that
-        //it WILL NOT work
-        //fuck anchor points fuck reversed coordinate bullshit it does
     }
 
-    private void ShowTooltipLocal(string tooltipText)
+    public void ShowTooltip(string tooltipText)
     {
         gameObject.SetActive(true);
         SetText(tooltipText);
     }
-    private void HideTooltipLocal()
+    public void HideTooltip()
     {
+        SetText("Error after inactive state");
         gameObject.SetActive(false);
-        SetText("");
-    }
-
-    public static void ShowTooltip(string tooltipText)
-    {
-        instance.ShowTooltipLocal(tooltipText);
-    }
-    public static void HideTooltip()
-    {
-        instance.HideTooltipLocal();
     }
 }
