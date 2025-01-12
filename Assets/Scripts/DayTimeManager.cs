f9aa4d5a4cc3950404ae57fd34417297df571e6b
The Meowy <meowmeowingstorage00@gmail.com>
2025-01-12 13:45:12 +0300

    action added

--- Assets/Scripts/DayTimeManager.cs
+++ Assets/Scripts/DayTimeManager.cs
@@ -1,22 +1,23 @@
+using System;
 using UnityEngine;
 
 public class DayTimeManager : MonoBehaviour
 {
     private int m_dayCount=0;
-    //action or event here I guess
-    //public static ??event?? ?Action<>??? UnityAction??? OnDateTimeChange; //<- forgor how help
+    public event Action onDateTimeChange; //<- ????
 
     public void DayAdd()
     {
         m_dayCount++;
         Debug.Log($"day added, current day: {m_dayCount}");
-        //here give signal that day changed to whoever subscribed
+        //here give signal that day changed to whoever subscribed (сигнал для подписавшихся о смене дня)
+        onDateTimeChange?.Invoke();
     }
 
     public void DayResetCounter()
     {
         m_dayCount = 0;
-        //signal here too
+        //signal here too for reset
     }
     public int DayGetCounter()
     {
