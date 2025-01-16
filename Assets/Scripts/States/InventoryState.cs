5727949a84de5415ab7cafca85fd53643147e446
Anastasia <kanechik.k@gmail.com>
2025-01-16 13:15:21 +0300

    inventory ui bug fix

--- Assets/Scripts/States/InventoryState.cs
+++ Assets/Scripts/States/InventoryState.cs
@@ -41,10 +41,10 @@ public class InventoryState : MonoBehaviour
         potionPanelSlots = potionsPanel.GetComponentsInChildren<UIInventoryItem>();
         List<InventorySlot> potions = GamePlayState.inventory.GetItemsByType(ItemCategory.Potion);
 
-        foreach (UIInventoryItem slot in potionPanelSlots)
-        {
-            slot.item = sampleItem;
-        }
+        // foreach (UIInventoryItem slot in potionPanelSlots)
+        // {
+        //     slot.InitialiseItem(sampleItem);
+        // }
 
         // Если слот пустой, то в него кладем сэмпл, если нет, то зелье
         for (int i = 0; i < Math.Min(potionPanelSlots.Length, potions.Count); i++)
@@ -52,21 +52,21 @@ public class InventoryState : MonoBehaviour
             // Убираем все нулы чтобы не возникали ошибки при вызове картинки айтема
             if (potions[i].item == null)
             {
-                potionPanelSlots[i].item = sampleItem;
+                potionPanelSlots[i].InitialiseItem(sampleItem);
             }
             else
             {
-                potionPanelSlots[i].item = potions[i].item;
+                potionPanelSlots[i].InitialiseItem(potions[i].item);
             }
         }
 
         ingredientPanelSlots = ingredientsPanel.GetComponentsInChildren<UIInventoryItem>();
         List<InventorySlot> ingredients = GamePlayState.inventory.GetItemsByType(ItemCategory.Ingredient);
 
-        foreach (UIInventoryItem slot in ingredientPanelSlots)
-        {
-            slot.item = sampleItem;
-        }
+        // foreach (UIInventoryItem slot in ingredientPanelSlots)
+        // {
+        //     slot.item = sampleItem;
+        // }
 
         for (int i = 0; i < Math.Min(ingredientPanelSlots.Length, ingredients.Count); i++)
         {
@@ -83,20 +83,20 @@ public class InventoryState : MonoBehaviour
         seedPanelSlots = seedsPanel.GetComponentsInChildren<UIInventoryItem>();
         List<InventorySlot> seeds = GamePlayState.inventory.GetItemsByType(ItemCategory.Seed);
 
-        foreach (UIInventoryItem slot in seedPanelSlots)
-        {
-            slot.item = sampleItem;
-        }
+        // foreach (UIInventoryItem slot in seedPanelSlots)
+        // {
+        //     slot.InitialiseItem(sampleItem);
+        // }
 
         for (int i = 0; i < Math.Min(seedPanelSlots.Length, seeds.Count); i++)
         {
             if (seeds[i].item == null)
             {
-                seedPanelSlots[i].item = sampleItem;
+                seedPanelSlots[i].InitialiseItem(sampleItem);
             }
             else
             {
-                seedPanelSlots[i].item = seeds[i].item;
+                seedPanelSlots[i].InitialiseItem(seeds[i].item);
             }
         }
     }
