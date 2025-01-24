da4503a80f351deba57d614278ef3e99c4107f7f
vladikNovla <zyymer20001000@gmail.com>
2025-01-24 16:45:41 +0300

    susi

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,21 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Inventory playerInventory;
+    public ObjectiveManager objectiveManager;
+    public QuestInfo objectiveInfo;
+    public Objective objective;
+
+    private void Awake()
     {
-        
+        objectiveInfo = Resources.Load<QuestInfo>("ScriptableObjects/MakeOneHealthPotion");
+        objective = new Objective(objectiveInfo.EventTrigger, objectiveInfo.StatusText, objectiveInfo.MaxValue);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        objectiveManager.AddObjective(objective);
     }
 }
