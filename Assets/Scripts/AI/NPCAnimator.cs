83f9ee277d956e913a82137bfe40c97f9cabfd98
vladikNovla <zyymer20001000@gmail.com>
2025-02-04 19:51:22 +0300

    added npc animation

---
+++ Assets/Scripts/AI/NPCAnimator.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class NPCAnimator : MonoBehaviour
+{
+    private Animator m_animator;
+    private Vector3 m_lastPosition;
+    private Transform m_thisTransform;
+
+    private void Awake()
+    {
+        m_thisTransform = transform;
+        m_animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        m_lastPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        Vector3 thisPosition = m_thisTransform.position;
+        float speed = Vector3.Distance(m_lastPosition, thisPosition) / Time.deltaTime;
+        if (speed > 0.5)
+        {
+            m_animator.SetBool("Walking", true);
+        }
+        else
+        {
+            m_animator.SetBool("Walking", false);
+        }
+
+        m_lastPosition = thisPosition;
+
+    }
+
+}
