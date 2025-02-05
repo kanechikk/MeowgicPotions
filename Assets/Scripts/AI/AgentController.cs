e1088977976f933ecbde6992fd74df923c54772d
vladikNovla <zyymer20001000@gmail.com>
2025-02-05 21:30:16 +0300

    Collider on agent

--- Assets/Scripts/AI/AgentController.cs
+++ Assets/Scripts/AI/AgentController.cs
@@ -1,12 +1,27 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class AgentController : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent m_navMeshAgent;
-    
+    public Action onStopped;
+    public Action onGoing;
+
     public void GoToDestination(Vector3 destinationPoint)
     {
         m_navMeshAgent.SetDestination(destinationPoint);
+    }
+
+    private void Update()
+    {
+        if (m_navMeshAgent.velocity == Vector3.zero)
+        {
+            onStopped?.Invoke();
+        }
+        else
+        {
+            onGoing?.Invoke();
+        }
     }
 }
