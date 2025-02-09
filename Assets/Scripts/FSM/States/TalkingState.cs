625c9ec9824f5d8d31298b82ce342faa5a598075
vladikNovla <zyymer20001000@gmail.com>
2025-02-09 17:52:42 +0300

    cinemachine

--- Assets/Scripts/FSM/States/TalkingState.cs
+++ Assets/Scripts/FSM/States/TalkingState.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
+using Unity.Cinemachine;
 
 public class TalkingState : GameStateBehaviour
 {
-    
+	[SerializeField] private CinemachineCamera m_counterCamera;
+
+	private void OnEnable()
+	{
+		m_counterCamera.Priority = 100;
+	}
+
+	private void OnDisable()
+	{
+		m_counterCamera.Priority = 0;
+	}
 }
