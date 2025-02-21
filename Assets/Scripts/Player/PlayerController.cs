85d08a5aebab3f0c0b556a18435344e660589c6c
vladikNovla <zyymer20001000@gmail.com>
2025-02-22 00:03:49 +0300

    merge of the scenes complete

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -41,7 +41,24 @@ public class PlaeyrConroller : MonoBehaviour
         {
             m_states.GoToWatering();
         }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                // whatever tag you are looking for on your game object
+                
+                    Debug.Log($"---> Hit:  {hit.collider.name}");
+                
+            }
+        }
     }
+
+
 
     public void PointerDown()
     {
