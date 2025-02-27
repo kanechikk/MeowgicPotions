e60d511c5deb08efdcb51753268b7781772613c2
vladikNovla <zyymer20001000@gmail.com>
2025-02-27 21:33:14 +0300

    days transition screen (not complete)

--- Assets/Scripts/UI/Animations/UIAnimation.cs
+++ Assets/Scripts/UI/Animations/UIAnimation.cs
@@ -1,21 +1,39 @@
 using UnityEngine;
 using DG.Tweening;
+using System.Collections;
 
 public class UIAnimation : MonoBehaviour
 {
-    [SerializeField] private float m_fadeTime = 1f;
-    [SerializeField] private CanvasGroup m_canvasGroup;
+    [SerializeField] private float m_fadeTime = 2f;
+    [SerializeField] RectTransform m_sunMoonPanel;
     [SerializeField] RectTransform m_rectTransform;
+    [SerializeField] private Ease m_ease;
+    private Tween m_tween;
 
-    public void PanelFadeIn()
+    public Tween PanelFadeIn()
     {
-        m_rectTransform.transform.localPosition = new Vector3(0f, -1000f, 0f);
-        m_rectTransform.DOAnchorPos(new Vector2(0f, 0f), m_fadeTime, false).SetEase(Ease.OutElastic);
+        m_rectTransform.transform.localPosition = new Vector3(0f, -2000f, 0f);
+        return m_rectTransform.DOAnchorPos(new Vector2(0f, 0f), m_fadeTime, false).SetEase(m_ease).SetUpdate(true);
     }
 
-    public void PanelFadeOut()
+    private IEnumerator Sequence(Tween tween)
+    {
+        yield return tween.WaitForCompletion();
+    }
+    
+    public Tween PanelFadeOut()
     {
         m_rectTransform.transform.localPosition = new Vector3(0f, 0f, 0f);
-        m_rectTransform.DOAnchorPos(new Vector2(0f, -1000f), m_fadeTime, false).SetEase(Ease.InOutQuint);
+        return m_rectTransform.DOAnchorPos(new Vector2(0f, -1000f), m_fadeTime, false).SetEase(m_ease).SetUpdate(true);
+    }
+
+    private Tween SunMoonExchange()
+    {
+        return m_sunMoonPanel.DOShapeCircle(new Vector2(0f, 0f), 180f, m_fadeTime);
+    }
+
+    private void OnEnable()
+    {
+        
     }
 }
