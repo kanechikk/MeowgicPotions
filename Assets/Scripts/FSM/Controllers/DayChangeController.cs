9a3a67bb089a4dfe5159b78d3568bfdd0be5b7ff
vladikNovla <zyymer20001000@gmail.com>
2025-03-01 14:24:36 +0300

    fix time

--- Assets/Scripts/FSM/Controllers/DayChangeController.cs
+++ Assets/Scripts/FSM/Controllers/DayChangeController.cs
@@ -16,7 +16,7 @@ public class DayChangeController : MonoBehaviour
 
     public IEnumerator SavingInfo()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSecondsRealtime(1f);
 
         m_gameManager.SaveData();
         m_questManager.SaveQuestInfo();
